83e4274061d1499b2048ff47668a3d1a0b4e98ed
brunobog <bog906@gmail.com>
2019-08-02 15:02:50 -0300

    change to npm

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  publicPath: isProd ? "/knewin-social-dashboard/" : "",
+  publicPath: isProd ? "/Test-Vue/" : "",
   configureWebpack: {
     // Set up all the aliases we use in our app.
     plugins: [
@@ -21,7 +21,6 @@ module.exports = {
   css: {
     // Enable CSS source maps.
     sourceMap: process.env.NODE_ENV !== 'production'
-    ? '/Test-Vue/'
-    : '/'
+    
   }
 };
