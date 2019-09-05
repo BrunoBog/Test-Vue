5a1d55e3f0f8f3e11b275e6d99e0be2ee94847ae
brunobog <bog906@gmail.com>
2019-09-05 16:05:46 -0300

    change key

--- src/configs.js
+++ src/configs.js
@@ -1,6 +1,5 @@
 let config = {
     server : "https://localhost:44320/",
-    mgIDLoginAPI: "383624888075-094vd5gmnriro2kg67073tt5pi0rrm5o.apps.googleusercontent.com",
     gIDLoginAPI: " 388825528720-7kr3m6sqohpg82nre8o6v2ofjch1gpgi.apps.googleusercontent.com "
 };
 
