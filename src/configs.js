218b14ab0bba19f878193a9a22cd4f828e8d63d2
brunobog <bog906@gmail.com>
2019-09-06 09:44:09 -0300

    add a /

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,5 @@
 let config = {
-    server : "https://44b5a311.ngrok.io", 
+    server : "https://44b5a311.ngrok.io/", 
     gIDLoginAPI: " 388825528720-7kr3m6sqohpg82nre8o6v2ofjch1gpgi.apps.googleusercontent.com "
 };
 
