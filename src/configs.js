1cbfbd4569462350280ed62b032542e012918bbd
brunobog <bog906@gmail.com>
2019-09-06 07:54:00 -0300

    Alter server

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,5 @@
 let config = {
-    server : "http://45.7.168.64:8085/",
+    server : "http://45.7.168.64:8085/", 
     gIDLoginAPI: " 388825528720-7kr3m6sqohpg82nre8o6v2ofjch1gpgi.apps.googleusercontent.com "
 };
 
