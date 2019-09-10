a850124584aca056329b536d1371f44cc5a40709
brunobog <bog906@gmail.com>
2019-09-10 10:55:36 -0300

    Fix server

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,5 @@
 let config = {
-    server : "https://40764057.ngrok.io", 
+    server : "https://6c62a515.ngrok.io", 
     gIDLoginAPI: " 388825528720-7kr3m6sqohpg82nre8o6v2ofjch1gpgi.apps.googleusercontent.com "
 };
 
