cd0c71ccfb509cff5411751f5840533b4cca9055
Bruno Josafat <bruno.josafat@knewin.com>
2019-10-04 09:56:55 -0300

    rollback server

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,5 @@
 let config = {
-    server : "https://social.api.zubit.com.br:8080/", 
+    server : "http://45.7.168.64:8085/", 
     gIDLoginAPI: " 388825528720-7kr3m6sqohpg82nre8o6v2ofjch1gpgi.apps.googleusercontent.com "
 };
 
