3bb28e9e99fe057eeec2719217ae786cc2a81dfe
Bruno Josafat <bruno.josafat@knewin.com>
2019-10-04 10:03:38 -0300

    s

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,5 @@
 let config = {
-    server : "http://45.7.168.64:8085/", 
+    server : "https://bd06e6ec.ngrok.io/", 
     gIDLoginAPI: " 388825528720-7kr3m6sqohpg82nre8o6v2ofjch1gpgi.apps.googleusercontent.com "
 };
 
