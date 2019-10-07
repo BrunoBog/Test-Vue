c62c075a5e435a10cc527e8c020db8533dbed184
Bruno Josafat <bruno.josafat@knewin.com>
2019-10-07 09:11:00 -0300

    change server

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,5 @@
 let config = {
-    server : "https://social.api.zubit.com.br:8080/", 
+    server : "https://homologacao.social.api.zubit.com.br:8086/", 
     gIDLoginAPI: " 388825528720-7kr3m6sqohpg82nre8o6v2ofjch1gpgi.apps.googleusercontent.com "
 };
 
