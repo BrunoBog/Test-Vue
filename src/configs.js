7d7a36cbdd445ac71f164f1d7b4b660b4b7c778d
Bruno Josafat <bruno.josafat@knewin.com>
2019-10-21 09:59:16 -0300

    \o/

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,5 @@
 let config = {
-    server : "https://homologacao.social.api.zubit.com.br:8086/", 
+    server : "https://homologacao.social.api.zubit.com.br:8085/", 
     gIDLoginAPI: " 388825528720-7kr3m6sqohpg82nre8o6v2ofjch1gpgi.apps.googleusercontent.com "
 };
 
