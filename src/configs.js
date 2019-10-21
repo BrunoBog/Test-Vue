5e12e21c05c23e385c4119438a4e1fb78c8d5196
Bruno Josafat <bruno.josafat@knewin.com>
2019-10-21 08:45:06 -0300

    Add domain url

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,5 @@
 let config = {
-    server : "http://45.7.168.64:8085/", 
+    server : "https://homologacao.social.api.zubit.com.br:8086/", 
     gIDLoginAPI: " 388825528720-7kr3m6sqohpg82nre8o6v2ofjch1gpgi.apps.googleusercontent.com "
 };
 
