a92fc2856109777eac8089287d864f89d863b4e8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-04 12:58:47 +0000

    Deploy BrunoBog/Test-Vue to github.com/BrunoBog/Test-Vue.git:gh-pages

---
+++ precache-manifest.4df3e952c65e5b90b9f3fdb81613bec7.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd4a0d990cff02127c9f",
+    "url": "/Test-Vue/css/app.b8966a0c.css"
+  },
+  {
+    "revision": "cd4a0d990cff02127c9f",
+    "url": "/Test-Vue/js/app.0e604df5.js"
+  },
+  {
+    "revision": "04c1ef4a7c2cab410694",
+    "url": "/Test-Vue/js/chunk-vendors.abe4853c.js"
+  },
+  {
+    "revision": "f8bb09b2e564c16c0a9d",
+    "url": "/Test-Vue/css/demo.99dac8f3.css"
+  },
+  {
+    "revision": "f8bb09b2e564c16c0a9d",
+    "url": "/Test-Vue/js/demo.0700534e.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/Test-Vue/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/Test-Vue/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/Test-Vue/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/Test-Vue/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/Test-Vue/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "bbeee2bfe1ebc32a955c8675a43d2a07",
+    "url": "/Test-Vue/index.html"
+  },
+  {
+    "revision": "a794a15f6e45b941d8a521e7965221a8",
+    "url": "/Test-Vue/img/brand/favicon.png"
+  },
+  {
+    "revision": "59d7c2b119ab46fc7b5f853f04324c89",
+    "url": "/Test-Vue/img/brand/green.png"
+  },
+  {
+    "revision": "2394741c50efda8ce9a3f5524492cadf",
+    "url": "/Test-Vue/img/brand/Kn_social@2x.png"
+  },
+  {
+    "revision": "e0ea3f1916671872498288dc2bd40f06",
+    "url": "/Test-Vue/img/brand/white.png"
+  },
+  {
+    "revision": "1f533d2fdc40493ee834dd8e4288220c",
+    "url": "/Test-Vue/img/theme/angular.jpg"
+  },
+  {
+    "revision": "d136a071cf09ba5ab8dc2ada1ab04015",
+    "url": "/Test-Vue/img/theme/bootstrap.jpg"
+  },
+  {
+    "revision": "50dd5fdc839dfc16739b15370650485c",
+    "url": "/Test-Vue/img/theme/react.jpg"
+  },
+  {
+    "revision": "3b8c576082e5ca27bae21a839f1290f6",
+    "url": "/Test-Vue/img/theme/sketch.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/Test-Vue/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/Test-Vue/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/Test-Vue/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/Test-Vue/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "c532f6f0176968caa6265864ee1abb35",
+    "url": "/Test-Vue/img/theme/vue.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Test-Vue/robots.txt"
+  },
+  {
+    "revision": "cee60cfd101f9b3d2aae0e819ec8b267",
+    "url": "/Test-Vue/img/theme/profile-cover.jpg"
+  }
+];
